d07ee0e35a2889248c49a82fee077c5b7c0480d8
kckhafra <kckhafra@gmail.com>
2021-11-17 12:33:10 -0500

    initial commit

--- ZK_equality.py
+++ ZK_equality.py
@@ -6,12 +6,15 @@ def ZK_equality(G,H):
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
     r1 = Secrete(utils.get_random_num(bits=128))
     r2 = Secrete(utils.get_random_num(bits=128))
+    print(r1,r2)
     #Generate a NIZK proving equality of the plaintexts
     m=1
-    C1 = r1.value * GamalC2 = m * G + r1.value *H
-    C2 = m*G + r1.value * H
+    C1 = r1.value * G
+    C2 = r1.value * H + m *G
     #Return two ciphertexts and the proof
     D1 = r2.value*G
     D2 = m*G+r2.value *H
     return (C1,C2), (D1,D2), zk_proof
 
+# # Setup: Peggy and Victor agree on two group generators.
+# G, H = utils.make_generators(num=2, seed=42)
