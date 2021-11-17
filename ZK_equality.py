ec2aaf35a3ebe2ed957fa9b756db5a1c60e5d7c1
kckhafra <kckhafra@gmail.com>
2021-11-17 12:35:26 -0500

    intial

--- ZK_equality.py
+++ ZK_equality.py
@@ -4,8 +4,8 @@ from zksk import utils
 def ZK_equality(G,H):
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
-    r1 = Secrete(utils.get_random_num(bits=128))
-    r2 = Secrete(utils.get_random_num(bits=128))
+    r1 = Secret(utils.get_random_num(bits=128))
+    r2 = Secret(utils.get_random_num(bits=128))
     print(r1,r2)
     #Generate a NIZK proving equality of the plaintexts
     m=1
